b15acacd82475a66559d14d5702f4b7c780bf566
Volodymyr Mikhaylyk <decquart@gmail.com>
2017-12-23 15:13:23 +0200

    added application context

---
+++ DataAccessLayer/Context/ApplicationContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using DataAccessLayer.Entities;
+
+
+
+namespace DataAccessLayer.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<IncomeCategory> IncomeCategories { get; set; }
+        public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+
+        static ApplicationContext()
+        {
+            Database.SetInitializer<ApplicationContext>(new WalletDbInitializer());
+        }
+        public ApplicationContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+
+    public class WalletDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext db)
+        {
+            db.Wallets.Add(new Wallet { Id = 1, Name = "Main Wallet"});            
+            db.SaveChanges();
+        }
+    }
+}
