e83f3a689781440f013c9c8e87a3d52c204bbe15
decquart <decquart@gmail.com>
2018-01-22 22:27:39 +0200

    fixed bugs, refactored code

--- DataAccessLayer/Entities/Expense.cs
+++ DataAccessLayer/Entities/Expense.cs
@@ -8,13 +8,12 @@ namespace DataAccessLayer.Entities
         public int Id { get; set; }
         public double Sum { get; set; }
         public string Comment { get; set; }
-        public ExpenseCategory category { get; set; }
         public DateTime OperationTime { get; set; }
 
         public int WalletID { get; set; }
         public Wallet Wallet { get; set; }
 
         public int ExpenseCategoryID { get; set; }
-        public ExpenseCategory ExpenseCategory { get; set; }
+        public ExpenseCategory category { get; set; }
     }
 }
--- DataAccessLayer/Entities/Income.cs
+++ DataAccessLayer/Entities/Income.cs
@@ -8,13 +8,12 @@ namespace DataAccessLayer.Entities
         public int Id { get; set; }
         public double Sum { get; set; }
         public string Comment { get; set; }
-        public IncomeCategory category { get; set; }
         public DateTime OperationTime { get; set; }
 
         public int WalletID { get; set; }
         public Wallet Wallet { get; set; }
 
         public int IncomeCategoryID { get; set; }
-        public IncomeCategory IncomeCategory { get; set; }
+        public IncomeCategory category { get; set; }
     }
 }
