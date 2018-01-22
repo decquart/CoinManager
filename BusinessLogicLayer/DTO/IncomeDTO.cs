9121f525c725e9b43bcfb4b701784a6a4f8465f9
decquart <decquart@gmail.com>
2018-01-22 22:37:21 +0200

    created Data Transfer Objects

---
+++ BusinessLogicLayer/DTO/IncomeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.DTO
+{
+    public class IncomeDTO
+    {
+        public int Id { get; set; }
+        public double Sum { get; set; }
+        public string Comment { get; set; }
+        public DateTime OperationTime { get; set; }
+
+        public int WalletID { get; set; }
+        public int IncomeCategoryID { get; set; }
+    }
+}
