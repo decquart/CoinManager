025b9494b5877f8d049c292ec5e913c121a16d48
decquart <decquart@gmail.com>
2018-01-24 16:29:22 +0200

    created class which will send exceptions to presentation layer

---
+++ BusinesLigicLayer/Infrastructure/ValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace BusinessLogicLayer.Infrastructure
+{
+    public class ValidationException : Exception
+    {
+        public string Property { get; protected set; }
+        public ValidationException(string prop)
+        {
+            Property = prop;
+        }
+    }
+}
