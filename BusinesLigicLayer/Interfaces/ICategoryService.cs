cd9bfa794ffda43882d42552b2f9d82c2a6ec313
decquart <decquart@gmail.com>
2018-01-24 16:19:06 +0200

    defined interfaces for flexibility of services

---
+++ BusinesLigicLayer/Interfaces/ICategoryService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BusinessLogicLayer.DTO;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface ICategoryService
+    {
+        //create
+        void CreateIncomeCategory(string name);
+        void CreateExpenseCategory(string name);
+
+        //read
+        IEnumerable<ExpenseCategoryDTO> GetAllExpensesCategories();
+        IEnumerable<IncomeCategoryDTO> GetAllIncomesCategories();
+
+        //update
+        void ChangeIncomeCategoryName(int catId, string name);
+        void ChangeExpenseCategoryName(int catId, string name);
+
+        //delete
+        void RemoveIncomeCategory(int categoryId);
+        void RemoveExpenseCategory(int categoryId);
+
+        void Dispose();
+    }
+}
