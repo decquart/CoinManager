13e629ef97293bd53cfee323b2513f390cd8c2df
decquart <decquart@gmail.com>
2018-02-13 00:04:50 +0200

    added validation atributes

--- PresentationLayer/Models/WalletModel.cs
+++ PresentationLayer/Models/WalletModel.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace PresentationLayer.Models
 {
     public class WalletModel
     {
+        [HiddenInput(DisplayValue = false)]  
         public int Id { get; set; }
+
+
+        [Required]
+        [Range(1, 20000000)]
         public double Balance { get; set; }
+
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
     }
 }
